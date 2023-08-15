813dbbf4a87f77eec956d83f432bd750cf350c42
Yuri Garrido <y.garrido.yuri@gmail.com>
2023-08-15 16:24:21 -0300

    chore: intall knex

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
 
 app
   .listen({

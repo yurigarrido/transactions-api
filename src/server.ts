a72b4037a35acce2c325aa5002b9cf26ac36ba64
Yuri Garrido <y.garrido.yuri@gmail.com>
2023-08-15 17:37:33 -0300

    feat: validate env

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('HTTP server runing!'))

7724bc8cd28f656386855bc203fa35aa974f72b7
Yuri Miranda Garrido <y.garrido.yuri@gmail.com>
2025-02-26 11:16:53 -0300

    teste

--- src/database.ts
+++ src/database.ts
@@ -6,13 +6,11 @@ if (!process.env.DATABASE_URL) {
 }
 
 export const config: Knex.Config = {
-  client: env.DATABASE_CLIENT,
+  client: 'sqlite',
   connection:
-    env.DATABASE_CLIENT === 'sqlite'
-      ? {
-          filename: env.DATABASE_URL,
-        }
-      : env.DATABASE_URL,
+  {
+    filename: "./db/app.db",
+  },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import fastify from 'fastify';
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+app.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
   // if (err) {
   //   // console..log.error(err);
   //   process.exit(1);
